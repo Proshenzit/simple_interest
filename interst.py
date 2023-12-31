54ee4417a653cd639f5dd9bf361e5960c972258b
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2023-12-31 01:24:27 -0800

    Add files via upload

---
+++ interst.py
@@ -0,0 +1,7 @@
+p = int (input("Enter  your principle: "))
+r = int (input("Enter your   rate  of interst: "))
+t = int (input("Enter your  time period   year: "))
+si = (p*r*t)/100
+print("Enter  your somple  interest: ",si)
+a = p+si
+print("Your amount  is: ",a)
